fd9f62fa4b3042f16d4aeb1de876762e8370b9df
Amanda Duva <amanda.j.duva@gmail.com>
2023-06-15 14:42:35 -0400

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,60 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const path = reqire("path");
+const path = require("path");
 const generateMarkdownImport = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please type a short description of your project.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose what kind of license your project should have.",
+        choices: ["MIT", "Apache 2.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be used to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please type what the user needs to know about using the repository.",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can the user contribute to the repository?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be used to run tests?",
+        default: "npm test"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
